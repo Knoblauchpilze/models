10686ddd16b61739631f2594e1c18c61c843a790
Knoblauchpilze <prolific-programmer@tuta.com>
2022-10-03 11:40:55 +0200

    feat: Added loading behavior

--- src/game/simulation/Model.cc
+++ src/game/simulation/Model.cc
@@ -4,6 +4,18 @@
 
 namespace eqdif {
 
+  namespace {
+
+    unsigned
+    eatEndOfLine(std::ifstream& in) {
+      std::string dummy;
+      std::getline(in, dummy);
+
+      return dummy.size();
+    }
+
+  }
+
   Model::Model():
     utils::CoreObject("model"),
     Process()
@@ -13,14 +25,79 @@ namespace eqdif {
     m_variableNames.push_back("haha");
     m_initialValues.push_back(0.2f);
 
+    m_variableNames.push_back("hihi");
+    m_initialValues.push_back(-1.9f);
+
     m_values.push_back({0.2f, 0.5f});
     m_values.push_back({0.4f, 0.6f});
   }
 
   void
   Model::load(const std::string& file) {
-    /// TODO: Handle load of the game.
-    warn("Should load game from " + file);
+    // Open the file and verify that it is valid.
+    std::ifstream in(file.c_str());
+    if (!in.good()) {
+      error(
+        "Failed to load model to \"" + file + "\"",
+        "Failed to open file"
+      );
+    }
+
+    // Read the number of variables.
+    unsigned count = 0u;
+    in >> count;
+
+    m_variableNames.clear();
+    m_initialValues.clear();
+
+    // Read all variables.
+    for (unsigned id = 0u ; id < count ; ++id) {
+      std::string name;
+      float initialValue = 0.0f;
+
+      in >> name;
+      in >> initialValue;
+
+      log("Loaded variable " + name + " with initial value " + std::to_string(initialValue));
+
+      m_variableNames.push_back(name);
+      m_initialValues.push_back(initialValue);
+    }
+
+    // Read simulation steps.
+    in >> count;
+
+    log("Will read " + std::to_string(count) + " step(s)");
+    eatEndOfLine(in);
+
+    m_values.clear();
+
+    for (unsigned id = 0u ; id < count ; ++id) {
+      std::vector<float> step;
+
+      for (unsigned val = 0u ; val < m_variableNames.size() ; ++val) {
+        float value = 0.0f;
+        in.read(reinterpret_cast<char*>(&value), sizeof(float));
+        step.push_back(value);
+      }
+
+      // Read the rest of the line.
+      if (auto discarded = eatEndOfLine(in); discarded > 0) {
+        // The division comes from the fact that in each step we expect
+        // floating point values for variables. The remaining characters
+        // are probably unknown variables.
+        warn("Discarded " + std::to_string(discarded / sizeof(float)) + " character(s) for step " + std::to_string(id));
+      }
+
+      m_values.push_back(step);
+      log("Read " + std::to_string(step.size()) + " value(s) for step " + std::to_string(id));
+    }
+
+    info(
+      "Loaded simulation with " + std::to_string(m_variableNames.size()) +
+      " variable(s) and " + std::to_string(m_values.size()) +
+      " simulation step(s) from " + file
+    );
   }
 
   void
